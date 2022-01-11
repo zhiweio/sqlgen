26d0a77a3e8483d28fbdd05a32a16c4b44bcd039
wangzhiwei <wangzhiwei@patsnap.com>
2022-01-11 19:34:51 +0800

    feat(web): support to delete cached documents

--- sqlgen/web.py
+++ sqlgen/web.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf8
-
+import time
 from pathlib import Path
 
 from pywebio import config
 from pywebio import start_server
 from pywebio.input import *
 from pywebio.output import *
-from pywebio.session import download, run_js, set_env
+from pywebio.pin import *
+from pywebio.session import download, go_app, run_js, set_env
 
 from sqlgen import ddlgenerator, reader
 from sqlgen.cli import parse_sheets
@@ -16,31 +17,76 @@ cache_dir = Path(__file__).parent.parent.joinpath(".cache")
 if not cache_dir.exists():
     cache_dir.mkdir(parents=True, exist_ok=True)
 
+toast_duration = 2
+
 
 def get_existing_docs():
     return list(cache_dir.glob("*.xlsx"))
 
 
-def btn_back():
+def refresh_page():
     run_js("window.location.reload()")
 
 
-def click_back():
-    put_button("Back", color="dark", onclick=btn_back, small=True)
+def go_back(callback=refresh_page):
+    put_button("Back", color="dark", onclick=callback, small=True)
 
 
-@config(theme="minty")
-def main():
+def page_header():
     set_env(title="sqlgen", auto_scroll_bottom=True)
-
+    put_link("Home\t\t", app="main")
+    put_link("Documents", app="documents")
     put_markdown(
         """
     # sqlgen
     Infers SQL DDL (Data Definition Language) from template file.
     
     details: http://git.patsnap.com/wangzhiwei/sqlgen
+    
     """
     )
+
+
+def delete_file(file: Path):
+    if isinstance(file, str):
+        file = Path(file)
+    file.unlink(missing_ok=True)
+    return file
+
+
+def delete(selected):
+    if not selected:
+        toast("No documents selected", duration=toast_duration, color="warning")
+        return
+    deleted = list()
+    for doc in selected:
+        ret = delete_file(doc)
+        deleted.append(ret.name)
+    toast(f"{','.join(deleted)} deleted", duration=toast_duration, color="success")
+    time.sleep(toast_duration)
+    refresh_page()
+
+
+@config(theme="minty")
+def documents():
+    page_header()
+    put_markdown(
+        f"""
+        ## documents manage
+    """
+    )
+    options = [{"label": _.name, "value": str(_)} for _ in get_existing_docs()]
+    if options:
+        put_checkbox(name="selected", options=options, label="Select documents")
+        put_button("Delete", onclick=lambda: delete(pin.selected), small=True)
+    else:
+        put_text("No documents exist")
+        go_back(lambda: go_app("home"))
+
+
+@config(theme="minty")
+def main():
+    page_header()
     inputs = [
         file_upload(
             "Upload new document:",
@@ -89,13 +135,13 @@ def main():
         doc_name = Path(template).name
         if not Path(template).exists():
             put_warning(f"missing file: {template}!")
-            click_back()
+            go_back()
             return
     else:
         doc = data["doc"]
         if not doc:
             put_warning("No file selected or uploaded!")
-            click_back()
+            go_back()
             return
         template = cache_dir.joinpath(doc["filename"])
         doc_name = doc["filename"]
@@ -113,7 +159,7 @@ def main():
         clauses = "\n".join(clauses)
     except Exception as e:
         put_error(e)
-        click_back()
+        go_back()
         return
 
     put_markdown(
@@ -122,7 +168,13 @@ def main():
     **sheets:** {sheets}
     """
     )
-    put_code(f"{clauses}", language="sql")
+    put_collapse(
+        "ddl.sql",
+        [
+            put_code(f"{clauses}", language="sql"),
+        ],
+        open=True,
+    )
 
     def btn_download():
         download("ddl.sql", clauses.encode("utf8"))
@@ -132,10 +184,15 @@ def main():
             dict(label="Download", value="download", color="primary"),
             dict(label="Back", value="back", color="dark"),
         ],
-        onclick=[btn_download, btn_back],
+        onclick=[btn_download, refresh_page],
         small=True,
     )
 
 
+@config(theme="minty")
+def index():
+    main()
+
+
 if __name__ == "__main__":
-    start_server(main, port=8080)
+    start_server([index, main, documents], port=8080)
